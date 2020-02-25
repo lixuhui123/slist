bc92cb4df1e670dd50cb4fb2695b2a814a3c074e
lixuhui123 <2871726754@qq.com>
2020-02-25 21:16:12 +0800

    一些更新

--- The data structure/main.cpp
+++ The data structure/main.cpp
@@ -1,77 +1,82 @@
+////#include<stdio.h>
+////#include<stdlib.h>
+////#include "seqList.h"
+////int main()
+////{
+////	SeqList test;
+////	SeqListInit(&test, 10);
+////    system("pause");
+////    return 0;
+////}
 //#include<stdio.h>
 //#include<stdlib.h>
-//#include "seqList.h"
+//#include <string.h>
+//#include "slist.h"
+///*
+//for(cur=head;cur;cur=cur->next)
+//
+//{
+//cur;
+//}
+//for(cur=head->next;cur!=head;cur=cur->next)
+//{
+////双向链表把next改成per就倒着遍历
+//cur;
+//}
+//for(cur=head;cur;cur=cur->next)
+//
+//{
+//cur;
+//}
+//*/
+//void listtest()
+//{
+//	SList test;
+//
+//	SListInit(&test);
+//
+//	SListPushFront(&test, 10);
+//	SListPushFront(&test, 0);
+//	SListPushFront(&test, 1);
+//	SListPushFront(&test, 2);
+//	SListPushFront(&test, 33);
+//	SListPushFront(&test, 4);
+//	SListPushFront(&test, 5);
+//	SListPushFront(&test, 6);
+//
+//	SListPushFront(&test, 1);
+//	SListPushFront(&test, 2);
+//	SListPushFront(&test, 3);
+//
+//
+//	SListNode* cur = SListFind(&test, 2);
+//	SListInsertAfter(cur, 20);
+//	SListEraseAfter(cur);
+//	SListPrint(&test);
+//
+//	SListRemove(&test, 4);
+//	SListPrint(&test);
+//
+//	SListreverse(&test);
+//	SListPrint(&test);
+//
+//	//SListPushFront(&test, 10);
+//	//SListPushFront(&test, 0);
+//	//SListPushFront(&test, 1);
+//	//SListPushFront(&test, 2);
+//	//SListPushFront(&test, 3);
+//	//SListPushFront(&test, 4);
+//	// 
+//	//SListMakeCycle(&test);
+//	//SListNode* target = NULL;
+//	//target = Find(&test);
+//	//printf("%d\n", target->_data);
+//	printf("%d ", sizeof(SListNode));
+//}
 //int main()
 //{
-//	SeqList test;
-//	SeqListInit(&test, 10);
-//    system("pause");
-//    return 0;
+//	listtest();
+//	 
+//	system("pause");
+//	return 0;
 //}
-#include<stdio.h>
-#include<stdlib.h>
-#include <string.h>
-#include "slist.h"
-/*
-for(cur=head;cur;cur=cur->next)
-
-{
-cur;
-}
-for(cur=head->next;cur!=head;cur=cur->next)
-{
-//双向链表把next改成per就倒着遍历
-cur;
-}
-for(cur=head;cur;cur=cur->next)
-
-{
-cur;
-}
-*/
-void listtest()
-{
-	SList test;
-
-	SListInit(&test);
-
-	SListPushFront(&test, 10);
-	SListPushFront(&test, 0);
-	SListPushFront(&test, 1);
-	SListPushFront(&test, 2);
-	SListPushFront(&test, 33);
-	SListPushFront(&test, 4);
-	SListPushFront(&test, 5);
-	SListPushFront(&test, 6);
-
-	SListNode* cur = SListFind(&test, 2);
-	SListInsertAfter(cur, 20);
-	SListEraseAfter(cur);
-	SListPrint(&test);
-
-	SListRemove(&test, 4);
-	SListPrint(&test);
-
-	SListreverse(&test);
-	SListPrint(&test);
-
-	//SListPushFront(&test, 10);
-	//SListPushFront(&test, 0);
-	//SListPushFront(&test, 1);
-	//SListPushFront(&test, 2);
-	//SListPushFront(&test, 3);
-	//SListPushFront(&test, 4);
-	// 
-	//SListMakeCycle(&test);
-	//SListNode* target = NULL;
-	//target = Find(&test);
-	//printf("%d\n", target->_data);
-	printf("%d ", sizeof(SListNode));
-}
-int main()
-{
-	listtest();
-	 
-	system("pause");
-	return 0;
-}
--- The data structure/slist.h
+++ The data structure/slist.h
@@ -1,33 +1,33 @@
-#pragma once
-#ifndef _SLIST_H
-#define _SLIST_H
-typedef int SLTDataType;
-typedef struct SListNode
-{
-	SLTDataType _data;
-	struct SListNode* _next;
-}SListNode;//链表节点，这是链表当中的单个块，包含了一个要存储的变量，和一个指针，当给_data
-//赋值的时候需要动态开辟个内存，因此链表使用的是堆空间
-typedef struct SList
-{
-	SListNode* _head;
-}SList;//链表，用它来初始化 SList test;plist=&test;用来记录头结点；
- //结构体的定义和初始化
-void SListInit(SList* plist);
-void SListDestory(SList* plist);
-//SListNode* BuySListNode(SLTDataType x);
-void SListPushFront(SList* plist, SLTDataType x);
-void SListPopFront(SList* plist);
-SListNode* SListFind(SList* plist, SLTDataType x);
-// 在pos的后面进行插入
-void SListInsertAfter(SListNode* pos, SLTDataType x);
-// 在pos的前面进行插入
-void SListEraseAfter(SListNode* pos);
-void SListRemove(SList* plist, SLTDataType x);
-int SListPopFrontJudge(SList* plist, SLTDataType x);
-void SListPrint(SList* plist);
-void SListreverse(SList* plist);
-SListNode* SListHasCycle1(SList * plist);
-SListNode* Find(SList* plist);
-void SListMakeCycle(SList* plist);
-#endif _SLIST_H
+//#pragma once
+//#ifndef _SLIST_H
+//#define _SLIST_H
+//typedef int SLTDataType;
+//typedef struct SListNode
+//{
+//	SLTDataType _data;
+//	struct SListNode* _next;
+//}SListNode;//链表节点，这是链表当中的单个块，包含了一个要存储的变量，和一个指针，当给_data
+////赋值的时候需要动态开辟个内存，因此链表使用的是堆空间
+//typedef struct SList
+//{
+//	SListNode* _head;
+//}SList;//链表，用它来初始化 SList test;plist=&test;用来记录头结点；
+// //结构体的定义和初始化
+//void SListInit(SList* plist);
+//void SListDestory(SList* plist);
+////SListNode* BuySListNode(SLTDataType x);
+//void SListPushFront(SList* plist, SLTDataType x);
+//void SListPopFront(SList* plist);
+//SListNode* SListFind(SList* plist, SLTDataType x);
+//// 在pos的后面进行插入
+//void SListInsertAfter(SListNode* pos, SLTDataType x);
+//// 在pos的前面进行插入
+//void SListEraseAfter(SListNode* pos);
+//void SListRemove(SList* plist, SLTDataType x);
+//int SListPopFrontJudge(SList* plist, SLTDataType x);
+//void SListPrint(SList* plist);
+//void SListreverse(SList* plist);
+//SListNode* SListHasCycle1(SList * plist);
+//SListNode* Find(SList* plist);
+//void SListMakeCycle(SList* plist);
+//#endif _SLIST_H
